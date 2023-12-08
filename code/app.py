63d430fff7da874a8b423dce785d20d180417535
ulricht01 <99655536+ulricht01@users.noreply.github.com>
2023-12-08 13:54:52 +0100

    0.1
    Napojení na databázi a automatizovaná tvorba databáze a tabulek včetně relací.

--- code/app.py
+++ code/app.py
@@ -1,20 +1,29 @@
 from flask import Flask
 from flask import render_template, redirect, jsonify
 import mariadb
+import database
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://root:secret@localhost/skoly'
 app.config['SECRET_KEY'] ="super secret key"
+
+#Vytvoří první connection do mariaDB a vytvoří db
+database.vytvor_db()
+
+#Pro manuální testování odkomentovat localhost a zakomentovat mariadb tady i v database.py
 
 config = {
         'user': 'root',
         'password': 'secret',
         'host': 'mariadb',
+        #'host': 'localhost',
         'port': 3306,
         'database': 'skoly'
     }
-connection = mariadb.connect(**config)
 
+connection = mariadb.connect(**config)
+cursor = mariadb.Cursor(connection)
+#Vytvoří druhý connection do mariaDB přímo do db a vytvoří tabulky
+database.vytvor_tabulky(cursor)
 
 @app.route('/')
 @app.route('/index')
---
+++ code/database.py
@@ -0,0 +1,84 @@
+import mariadb
+
+
+
+def vytvor_db():
+    config = {
+            'user': 'root',
+            'password': 'secret',
+            'host': 'mariadb',
+            #'host': 'localhost',
+            'port': 3306,
+        }
+    connection = mariadb.connect(**config)
+    cursor = mariadb.Cursor(connection)
+    cursor.execute("CREATE DATABASE IF NOT EXISTS skoly")
+
+def vytvor_tabulky(cursor):   
+    cursor.execute("""CREATE TABLE IF NOT EXISTS skoly(
+                                id_skola INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                nazev_skola VARCHAR(45) NOT NULL
+                            ) ENGINE=InnoDB;""")
+            
+    cursor.execute("""CREATE TABLE IF NOT EXISTS ucitele(
+                                id_ucitel INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                klic VARCHAR(15) NOT NULL UNIQUE,
+                                id_skola INT(6) NOT NULL,
+                                CONSTRAINT `fk_ucitele_skoly`
+                                    FOREIGN KEY (id_skola) REFERENCES skoly (id_skola)
+                                    ON DELETE CASCADE
+                                    ON UPDATE RESTRICT
+                            ) ENGINE=InnoDB;""")
+            
+    cursor.execute("""CREATE TABLE IF NOT EXISTS studenti(
+                                id_student INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                email VARCHAR(70) NOT NULL UNIQUE,
+                                klic VARCHAR(15) NOT NULL,
+                                id_skola INT(6) NOT NULL,
+                                CONSTRAINT `fk_studenti_skoly`
+                                    FOREIGN KEY (id_skola) REFERENCES skoly (id_skola)
+                                    ON DELETE CASCADE
+                                    ON UPDATE RESTRICT
+                            ) ENGINE=InnoDB;""")
+            
+    cursor.execute("""CREATE TABLE IF NOT EXISTS mistnosti(
+                                id_mistnost INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                nazev_mistnosti VARCHAR(10) NOT NULL,
+                                popis VARCHAR(125) NOT NULL,
+                                id_ucitel INT(6) NOT NULL,
+                                CONSTRAINT `fk_mistnosti_ucitele`
+                                    FOREIGN KEY (id_ucitel) REFERENCES ucitele (id_ucitel)
+                                    ON DELETE CASCADE
+                                    ON UPDATE RESTRICT
+                            ) ENGINE=InnoDB;""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS ukoly(
+                                id_ukol INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                nazev_ukol VARCHAR(25) NOT NULL,
+                                popis VARCHAR(250),
+                                id_mistnost INT(6) NOT NULL,
+                                CONSTRAINT `fk_ukoly_mistnosti`
+                                    FOREIGN KEY (id_mistnost) REFERENCES mistnosti (id_mistnost)
+                                    ON DELETE CASCADE
+                                    ON UPDATE RESTRICT
+                            ) ENGINE=InnoDB;""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS odevzdane_ukoly(
+                                id_ode_ukol INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                file BLOB NOT NULL,
+                                id_ukol INT(6) NOT NULL,
+                                id_mistnost INT(6) NOT NULL,
+                                id_student INT(6) NOT NULL,
+                                CONSTRAINT `fk_odevzdane_ukoly_ukoly`
+                                    FOREIGN KEY (id_ukol) REFERENCES ukoly (id_ukol)
+                                    ON DELETE CASCADE
+                                    ON UPDATE RESTRICT,
+                                CONSTRAINT `fk_odevzdane_ukoly_mistnosti`
+                                    FOREIGN KEY (id_mistnost) REFERENCES mistnosti (id_mistnost)
+                                    ON DELETE CASCADE
+                                    ON UPDATE RESTRICT,
+                                CONSTRAINT `fk_odevzdane_ukoly_studenti`
+                                    FOREIGN KEY (id_student) REFERENCES studenti (id_student)
+                                    ON DELETE CASCADE
+                                    ON UPDATE RESTRICT
+                            ) ENGINE=InnoDB;""")
