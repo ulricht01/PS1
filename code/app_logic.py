d0e5c619643d7296fb3f21548b581cfa8b9c5d99
Yorrii <hegerit@seznam.cz>
2024-02-16 18:38:22 +0100

    app_logic update
    přepsal jsem šifrování na werkzeug

--- code/app_logic.py
+++ code/app_logic.py
@@ -2,6 +2,7 @@ import random
 import string
 import os
 import hashlib
+from werkzeug.security import generate_password_hash
 from flask_login import UserMixin
 
 def generate_random_key(len=15):
@@ -10,17 +11,14 @@ def generate_random_key(len=15):
     return key
 
 def hash_email(email):
-    hashed_email = hashlib.sha256(email.encode('utf-8')).hexdigest()
+    hashed_email = generate_password_hash(email, method='sha256')
     return hashed_email
 
-def verify_email(user_input_email, stored_hashed_email):
-    hashed_input_email = hashlib.sha256(user_input_email.encode('utf-8')).hexdigest()
-    if hashed_input_email == stored_hashed_email:
-        return True
-    else:
-        return False
+def hash_klic(klic):
+    hashed_klic = generate_password_hash(klic, method='sha256')
+    return hashed_klic
     
 class User(UserMixin):
-    def __init__(self, klic, ucitel=False):
-        self.id = klic
+    def __init__(self, email, ucitel=False):
+        self.id = email
         self.jeUcitel = ucitel
